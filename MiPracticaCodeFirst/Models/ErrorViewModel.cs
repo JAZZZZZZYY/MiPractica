3469de0f31b96f92db414e0943c6e333499adc20
Jazmin Lue <MINEDUCYT@DESKTOP-GLJR0B3>
2025-03-12 07:17:56 -0600

    agregacion de codefirst

---
+++ MiPracticaCodeFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiPracticaCodeFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
